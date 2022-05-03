bf8f0bd2963257833da409ea5136b23e950f9cb2
Łukasz Roczniak <flufferinek@gmail.com>
2022-05-03 14:45:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def miles_conversion():
 
 window = Tk()
 window.title("Miles to kilometer converter.")
-window.config(padx=20, pady=20)
+window.config(padx=25, pady=25)
 
 miles_input = Entry(width=7)
 miles_input.grid(column=1, row=0)
@@ -28,3 +28,5 @@ kilometer_label.grid(column=2, row=1)
 
 calculate_button = Button(text="calculate", command=miles_conversion)
 calculate_button.grid(column=1, row=2)
+
+window.mainloop()
